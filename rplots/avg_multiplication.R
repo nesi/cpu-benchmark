65c836ea876265d49150d771d70b9c8cf8e6eb79
DininduSenanayake <dinindusen@gmail.com>
2024-08-19 14:35:51 +1200

    new R plot script

--- rplots/avg_multiplication.R
+++ rplots/avg_multiplication.R
@@ -8,10 +8,10 @@ library(tidyr)
 data <- read_csv("average-multiplication.csv")
 
 # Reshape the data from wide to long format
-data_long <- pivot_longer(data, cols = c(Genoa, Milan), names_to = "City", values_to = "Time")
+data_long <- pivot_longer(data, cols = c(Genoa, Milan), names_to = "microarchitecture", values_to = "Time")
 
 # Create the line chart
-ggplot(data_long, aes(x = ntasks, y = Time, color = City)) +
+ggplot(data_long, aes(x = ntasks, y = Time, color = microarchitecture)) +
   geom_line() +
   geom_point(size = 3) +
   gghighlight() +
@@ -21,5 +21,7 @@ ggplot(data_long, aes(x = ntasks, y = Time, color = City)) +
        x = "Number of Threads  ( OMP_NUM_THREADS)",
        y = "Average time per multiplication: (seconds)") +
   theme_minimal() +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom",
+        panel.background = element_rect(fill = "gray95", color = NA),
+        plot.background = element_rect(fill = "white", color = NA))
 
